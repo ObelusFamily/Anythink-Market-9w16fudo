cadb7e3a867558bf8740e88d440425bbb7e417eb
Jawadaz <Jawadaz@gmail.com>
2022-12-29 19:19:05 +0000

    Adding the title to the backend items filter

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -106,6 +106,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         tag: Optional[str] = None,
         seller: Optional[str] = None,
         favorited: Optional[str] = None,
+        title: Optional[str] = None,
         limit: int = 20,
         offset: int = 0,
         requested_user: Optional[User] = None,
@@ -195,6 +196,16 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
                     )
                 ),
             )
+            # fmt: on
+
+        if title:
+            query_params.append(title)
+            query_params_count += 1
+
+            # fmt: off
+            query = query.where(
+                    items.title.find(Parameter(query_params_count)) > 0,
+                ) 
             # fmt: on
 
         query = query.limit(Parameter(query_params_count + 1)).offset(
